df46e3ba97690606ad266dfc57cf6199cbd88891
gbcxlw <45224060+gbcxlw@users.noreply.github.com>
2021-10-10 21:33:23 +0800

    leetcode 46-全排列

--- leetcode/src/main/java/com/dataStructure/leetcode/回溯/_46_全排列.java
+++ leetcode/src/main/java/com/dataStructure/leetcode/回溯/_46_全排列.java
@@ -1,7 +1,6 @@
 package com.dataStructure.leetcode.回溯;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class _46_全排列 {
@@ -40,9 +39,42 @@ public class _46_全排列 {
                 path.add(nums[i]);
                 used[i] = true;
                 dfs(result, used, depth + 1, len, path, nums);
+                //下面的代码为回溯
                 used[i] = false;
                 path.remove(path.size() - 1);
             }
         }
+    }
+
+    public List<List<Integer>> permute2(int[] nums) {
+        if (nums == null || nums.length == 0 ) {
+            return null;
+        }
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums.length == 1) {
+            List<Integer> list = new ArrayList();
+            list.add(nums[0]);
+            result.add(list);
+            return result;
+        }
+        for (int i = 0; i < nums.length; i ++) {
+            int []nums2 = new int[nums.length - 1];
+            int j1 = 0;
+            int j2 = 0;
+            while (j1 < nums.length) {
+                if (nums[j1] != nums[i]) {
+                    nums2[j2] = nums[j1];
+                    j2 ++;
+                }
+                j1 ++;
+            }
+            List<List<Integer>> lastResult = permute(nums2);
+            for (List<Integer> tmpList : lastResult) {
+                tmpList.add(nums[i]);
+                result.add(tmpList);
+            }
+        }
+        return result;
+
     }
 }
